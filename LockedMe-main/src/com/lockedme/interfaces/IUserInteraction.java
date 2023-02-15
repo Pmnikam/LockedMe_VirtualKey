e1a14ee936e78544603a8ae7775f1dc569a469bf
Pmnikam <Pmnikam@users.noreply.github.com>
2023-02-16 00:35:19 +0530

    Delete LockedMe-main directory

--- LockedMe-main/src/com/lockedme/interfaces/IUserInteraction.java
+++
@@ -1,9 +0,0 @@
-package com.lockedme.interfaces;
-
-import java.util.Scanner;
-
-public interface IUserInteraction 
-{
-	public static String GetUserFileName(Scanner s) {return null;}
-	public static void InvalidInput() {}
-}
